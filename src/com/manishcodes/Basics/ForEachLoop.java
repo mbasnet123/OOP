e5c48c8245c41f9074c98d024dc5f1535f0fcfc9
mbasnet123 <mbasnet868@gmail.com>
2022-11-12 22:32:12 +0545

    added typecasting and foreach loop

---
+++ src/com/manishcodes/Basics/ForEachLoop.java
@@ -0,0 +1,20 @@
+package com.manishcodes.Basics;
+
+import java.util.ArrayList;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Manish");
+        names.add("Sunil");
+        names.add("Asis");
+        names.add("Rajesh");
+        for(String name:names) {
+            System.out.println("The name of the person is " + name);
+        }
+            int a[] = {1,2,4,3,5};
+            for(int b:a){
+                System.out.println(b+" ");
+            }
+        }
+    }
---
+++ src/com/manishcodes/Basics/TypeCasting.java
@@ -0,0 +1,17 @@
+package com.manishcodes.Basics;
+
+public class TypeCasting {
+        public static void main(String[] args) {
+            //implicit typecasting
+            int a = 5;
+            double b =a ;
+            System.out.println(b);
+
+            //explicit typecasting
+            double x = 3.5;
+            int y = (int) x;
+            System.out.println(y);
+            String dev = "Symb";
+
+        }
+}
